283db573b87f57f5ebc1304e2023e3717dc8df7b
seba3510 <seba3510@protonmail.com>
2025-05-23 18:01:25 -0400

    feat: add buttons that represent each tab

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,1 @@
-import "../styles/style.css";
-import { greeting } from "./restaurant.js";
-import televisionImage from "../assets/images/television.png";
-
-
-console.log(greeting);
-
-
-const image = document.createElement("img");
-image.src = televisionImage;
-
-document.body.appendChild(image);
+console.log("Hello, World!");
