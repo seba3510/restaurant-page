5874c8580a533d06579e19ba74115ffe59f4bfec
seba3510 <seba3510@protonmail.com>
2025-05-25 10:55:02 -0400

    feat: display header

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,6 +1,15 @@
 import "../styles/menu.css";
 
 //=======================================================================================
+
+import image1 from "../assets/images/left-bee.png";
+
+//=======================================================================================
+
+import image2 from "../assets/images/right-bee.png";
+
+//=======================================================================================
+
 class Menu {
 
 	#content;
@@ -20,17 +29,116 @@ class Menu {
 
 		this.#content.innerHTML = "";
 
-		const para =
-			document.createElement
-				("p");
-
-		para.textContent =
-			"Hello, World!";
-
-		this.#content.appendChild
-			(para);
+		this.#displayHeader();
 
 	} // display()
+
+	//=======================================================================================
+
+	#displayHeader() {
+		const header =
+			document.createElement
+				("header");
+
+		const outerContainer =
+			document.createElement
+				("section");
+
+		outerContainer.setAttribute
+			(
+				"class",
+				"outer-container"
+			);
+
+		header.appendChild
+			(outerContainer);
+
+		const innerContainer =
+			document.createElement
+				("section");
+
+		innerContainer.setAttribute
+			(
+				"class",
+				"inner-container"
+			);
+
+		header.appendChild
+			(innerContainer);
+
+		const img1 =
+			document.createElement
+				("img");
+
+		img1.setAttribute
+			(
+				"src",
+				image1
+			);
+
+		img1.setAttribute
+			(
+				"alt",
+				"This is a bee."
+			);
+
+		img1.setAttribute
+			(
+				"id",
+				"left-bee"
+			);
+
+		innerContainer.appendChild
+			(img1);
+
+		const h1 =
+			document.createElement
+				("h1");
+
+		h1.textContent =
+			"Menu";
+
+		innerContainer.appendChild
+			(h1);
+
+		const img2 =
+			document.createElement
+				("img");
+
+		img2.setAttribute
+			(
+				"src",
+				image2
+			);
+
+		img2.setAttribute
+			(
+				"alt",
+				"This is another bee."
+			);
+
+		img2.setAttribute
+			(
+				"id",
+				"right-bee"
+			);
+
+		innerContainer.appendChild
+			(img2);
+
+		outerContainer.appendChild
+			(innerContainer);
+
+		header.appendChild
+			(outerContainer);
+
+		this.#content.appendChild
+			(header);
+
+	} // displayHeader()
+
+	//=======================================================================================
+
 
 } // class
 
