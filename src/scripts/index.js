78759c1b970650b520cb05b5e9c372a08ab2f4e8
seba3510 <seba3510@protonmail.com>
2025-07-21 10:27:07 -0400

    chore: Configure webpack

---
+++ src/scripts/index.js
@@ -0,0 +1,14 @@
+import "../styles/style.css";
+
+//====================================================================
+
+import {
+	greeting
+} from "./greeting.js";
+
+//====================================================================
+
+import image from "../assets/images/frog.jpg"
+
+
+console.log(greeting);
