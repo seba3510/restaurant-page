8250130306c69248db7a5ee3314e89495cf06d51
seba3510 <seba3510@protonmail.com>
2025-08-05 13:21:06 -0400

    docs: update package.json

--- webpack.common.js
+++ webpack.common.js
@@ -2,9 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-	entry: {
-		app: "./src/index.js",
-	},
+	entry: "./src/index.js",
 	output: {
 		filename: "main.js",
 		path: path.resolve(__dirname, "dist"),
